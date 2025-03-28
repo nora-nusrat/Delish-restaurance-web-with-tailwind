7fbc500dfbbbf3b3caeb6e294d37eb8ef3b9b568
Nusrat Nora <fariyanushratnora@gmail.com>
2025-03-28 21:51:08 +0600

    Add files via upload
    Commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+AOS.init();
+
+
+
+
+let count = document.querySelectorAll(".count")
+let arr = Array.from(count)
+
+
+
+arr.map(function(item){
+  let startnumber = 0
+
+  function counterup(){
+  startnumber++
+  item.innerHTML= startnumber
+   
+  if(startnumber == item.dataset.number){
+      clearInterval(stop)
+  }
+}
+
+let stop =setInterval(function(){
+  counterup()
+},50)
+
+})
